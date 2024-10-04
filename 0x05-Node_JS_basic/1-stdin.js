721c7bbd462ef14eb8b94e2c1e3b6cffa83e7b5c
unknown <merykozan@gmail.com>
2024-10-04 22:53:57 +0100

    udate

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,16 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 if (process.stdin.isTTY) {
-  console.log('Welcome to Holberton School, what is your name?');
-  rl.question('', (name) => {
-    console.log(`Your name is: ${name}`);
-    rl.close();
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
   });
 } else {
-  let input = '';
   process.stdin.on('data', (data) => {
-    input += data;
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
   });
-  process.stdin.on('end', () => {
-    console.log('Welcome to Holberton School, what is your name?');
-    console.log(`Your name is: ${input.trim()}`);
-    console.log('This important software is now closing');
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
   });
 }
-rl.on('close', () => {
-  if (process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-});
